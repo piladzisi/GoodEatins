9a6b30fd9f1d8f309c7366430649a0a966ad4375
Anna <piladzisi@gmail.com>
2019-05-01 14:19:51 +0300

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by anna.sibirtseva on 01/05/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
