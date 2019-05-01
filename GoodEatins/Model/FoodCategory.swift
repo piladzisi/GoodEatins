f2b3f2615f92248faf20512c15875a3fe887e38a
Anna <piladzisi@gmail.com>
2019-05-01 22:12:06 +0300

    model and dataset

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by anna.sibirtseva on 01/05/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import Foundation
+
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by anna.sibirtseva on 01/05/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
