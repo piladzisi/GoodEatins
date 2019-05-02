38d8dbaa02b39d65180de720c8f161af828f959f
Anna <piladzisi@gmail.com>
2019-05-02 12:24:20 +0300

    categories Table View

--- GoodEatins/Controller/ViewController.swift
+++ GoodEatins/Controller/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    let data = DataSet()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-            }
-
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as? CategoryCell {
+            cell.configureCell(category: data.categories[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
 
 }
 
---
+++ GoodEatins/View/CategoryCell.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryCell.swift
+//  GoodEatins
+//
+//  Created by anna.sibirtseva on 02/05/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        categoryImage.layer.cornerRadius = 10
+    }
+    
+    func configureCell(category: FoodCategory) {
+        categoryImage.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
+    }
+}
